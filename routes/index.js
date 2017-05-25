d363d9d79e7f4bb938ce25751c36023654b9b3f0
RUTH <RUTHPARK@MIT.EDU>
2017-05-24 21:44:41 -0400

    styling

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'dynaMIT' });
 });
 
+router.get('/aboutus', function(req, res, next) {
+	res.render('aboutus', {});
+});
+
 module.exports = router;
